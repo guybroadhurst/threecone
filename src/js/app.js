75381246d7afa2e92474b867266bd37fdc812b9f
Guy Broadhurst <guy@guybroadhurst.co.uk>
2021-04-25 09:48:46 +0100

    Basic set up of three.js, added a cone

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import { Scene, WebGLRenderer } from 'three'
+import Cone from "./cone"
+import Camera from "./camera"
+
+const scene = new Scene()
+const camera = Camera()
+
+const renderer = new WebGLRenderer()
+renderer.setSize( window.innerWidth, window.innerHeight )
+document.body.appendChild( renderer.domElement )
+
+scene.add(Cone(0x676fb8))
+
+const animate = () => {
+	requestAnimationFrame( animate )
+	renderer.render( scene, camera )
+}
+animate()
